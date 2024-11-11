86e567a1c8b94752c098b4d941550d90539c265f
Jijigm <wjsddbswhl@naver.com>
2024-11-11 23:46:13 +0900

    genPrime 수정 및 주석 추가

--- RSA.c
+++ RSA.c
@@ -1,5 +1,16 @@
+/*
+	제목: RSA 알고리즘 구현
+    교과목: 정보보안개론
+    담당 교수: 박정민 교수
+    학번: 
+    이름: 
+    과제 제출일: 24.10.02
+*/
 #define _CRT_SECURE_NO_WARNINGS
 #define MAX 5
+#define MAXPRIME 200
+#define MINPRIME 100
+
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,24 +23,33 @@ typedef struct Queue {
     int rear;
 } QueueType;
 
-int genPrime(void) {
-    int randNum = rand();
+// 소수 판별
+int isPrime(int n) {
+    if (n < 2) return 0;
+    if (n == 2) return 1;
+    if (n % 2 == 0) return 0;
 
-    while (randNum < 100 || randNum > 200) {  // 랜덤생성되는 수의 범위지정
-        randNum = rand();
+    // 홀수만 판별
+    int limit = sqrt(n);
+    for (int i = 3; i <= limit; i += 2) {
+        if (n % i == 0) return 0;  // 합성수면 0 리턴
     }
-
-    for (int i = 2; i < randNum; i++) {  // 랜덤숫자가 소수가 아니면 다시 생성
-        if (randNum % i == 0) {
-            randNum = rand();
-            while (randNum < 100 || randNum > 200) randNum = rand();
-            i = 1;
-        }
-    }
-
-    return randNum;
+    return 1;  // 소수면 1 리턴
 }
 
+// 랜덤 소수 생성
+int genPrime() {
+    int p;
+
+    // MINPRIME ~ MAXPRIME 사이 소수 생성
+    do {
+        p = rand() % (MAXPRIME - MINPRIME + 1) + MINPRIME;
+    } while (!isPrime(p));
+
+    return p;
+}
+
+// 최대공약수 리턴
 int gcd(int p, int q) {
     int temp;
 
@@ -42,8 +62,10 @@ int gcd(int p, int q) {
     return p;
 }
 
+// 최소공배수 리턴
 int lcm(int p, int q) { return p * q / gcd(p, q); }
 
+// E 값을 구해서 리턴
 int genE(int L) {
     int num;
 
@@ -53,6 +75,7 @@ int genE(int L) {
     return -1;
 }
 
+// D 값을 구해서 리턴
 int genD(int E, int L) {
     int num;
 
@@ -62,6 +85,7 @@ int genD(int E, int L) {
     return -1;
 }
 
+// 고속 지수 모듈러 연산
 int mod_exp(int base, int exp, int mod) {
     int result = 1;
     base = base % mod;
@@ -77,12 +101,15 @@ int mod_exp(int base, int exp, int mod) {
     return result;
 }
 
+// 암호화
 int encryption(int plainText, int E, int N) { return mod_exp(plainText, E, N); }
 
+// 복호화
 int decryption(int cipherText, int D, int N) {
     return mod_exp(cipherText, D, N);
 }
 
+// 큐 구현 코드
 void initQueue(QueueType* q) {
     q->front = -1;
     q->rear = -1;
@@ -119,6 +146,7 @@ int dequeue(QueueType* q) {
     return value;
 }
 
+// main 함수
 int main(void) {
     int plainText, cipherText, decryptedText;
     int p, q, N, L, E, D;
@@ -134,32 +162,35 @@ int main(void) {
 
     srand(time(NULL));
 
+    // p, q 생성
     do {
         p = genPrime();
         q = genPrime();
     } while (p == q);
 
+    // N, L, E, D 생성
     N = p * q;
     L = lcm(p - 1, q - 1);
     E = genE(L);
     D = genD(E, L);
 
+    // 평문 입력 받기
     for (int i = 0; i < MAX; i++) {
         while (1) {
             printf("평문 입력(숫자로): ");
             scanf("%d", &plainText);  // 평문입력
             if (plainText > N)
-                printf("평문이 N보다 큽니다. 다시 입력하세요.\n");
+                printf("평문이 N(%d)보다 큽니다. 다시 입력하세요.\n", N);
             else
                 break;
         }
 
         enqueue(&plainTextQueue, plainText);  // 평문을 평문큐에 저장
 
-        cipherText = encryption(plainText, E, N);
+        cipherText = encryption(plainText, E, N);	// 암호문 생성
         enqueue(&cipherTextQueue, cipherText);
 
-        decryptedText = decryption(cipherText, D, N);
+        decryptedText = decryption(cipherText, D, N);	// 복호문 생성
         enqueue(&decryptedTextQueue, decryptedText);
     }
 
